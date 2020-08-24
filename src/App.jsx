b7284eff59fda1d01c4b7217d6c0b5e8dfa271f4
Avner Sorek <avner.sorek@gmail.com>
2020-08-24 09:57:19 +0300

    Adding React app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Welcome to the preview environment!</h1>
+      <p class="lead">Don't panic, it's just a preview (;</p>
+    </div>
+  </div>;
+}
+
+export default App;
