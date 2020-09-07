300156df869096b7e41310aad37540504aaa9d19
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2020-09-07 18:29:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return <div class="jumbotron jumbotron-fluid">
     <div class="container">
-      <h1 class="display-4">Welcome to the preview environment!</h1>
+      <h1 class="display-4">Welcome to the preview environment!!!!</h1>
       <p class="lead">Don't panic, it's just a preview (;</p>
     </div>
   </div>;
